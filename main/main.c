cd97acb479eea1f8205842785b4be2eeeb4e5988
ahmednust811 <ahmednust811@gmail.com>
2022-04-21 02:06:58 +0500

    initial commit

---
+++ main/main.c
@@ -0,0 +1,45 @@
+
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+
+
+#include "wifi_app.h"
+#include "wifi_reset_button.h"
+
+static const char TAG[] = "main";
+
+void wifi_application_connected_events(void)
+{
+	ESP_LOGI(TAG, "WiFi Application Connected!!");
+	
+	
+}
+
+void app_main(void)
+{
+    // Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+	{
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	// Start Wifi
+	wifi_app_start();
+ 
+
+	// Configure Wifi reset button
+	wifi_reset_button_config();
+
+	
+
+	// Set connected event callback
+	wifi_app_set_callback(&wifi_application_connected_events);
+
+	 // Start the DNS server that will redirect all queries to the softAP IP
+   
+}
+
